72befee33deeb0c00a0a3b8324b56bf59262e2b7
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2022-04-24 20:36:20 -0400

    Captive portal redirects in node

--- src/nodejs_app/app.js
+++ src/nodejs_app/app.js
@@ -4,7 +4,26 @@ const app = express();
 const port = 3000;
 
 // My modules
-const api = require('./api')
+const api = require('./api');
+
+// Captive portal redirect
+// Derive if the hostname is portal.pi.net, or an ip address
+app.all('*', function(req, res, next) {
+    // req.hostname stores hostname
+
+    // This is very crude. it matches "*.*.*.*" or "*.*.*.*:*", where * is a valid number (0-999 or 0-99999)
+    // OR portal.pi.net OR portal.pi.net:*
+    // OR localhost OR localhost:*
+    let ipv4Regex = /^(localhost|portal\.pi\.net|(\d{1,3}\.){3}\d{1,3})(|:\d{1,5})$/;
+
+    // Check if hostname is in ipv4Regex
+    if (ipv4Regex.test(req.hostname)) {
+        next();
+    } else {
+        // Redirect to "portal.pi.net"
+        res.redirect(302, 'http://portal.pi.net/');
+    }
+})
 
 // Static GET/HEAD requests
 app.head('/');
